0427074ba45cac9c9f74c6389752100aab1ef95a
qsfan <qsfan_vip@163.com>
2018-06-06 20:03:21 +0800

    增加ObjBean工具类，自由创建Bean对象

--- app/src/main/java/com/qsfan/qtils/MainActivity.java
+++ app/src/main/java/com/qsfan/qtils/MainActivity.java
@@ -7,10 +7,12 @@ import android.view.View;
 import android.widget.Button;
 
 import com.qsfan.qtils.rxjava.RxJavaRegisterActivity;
+import com.qsfan.qtils.utiltest.RetTestActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mBtRxjavaRequest;
+    private Button bt_rxjava_request;
+    private Button bt_ret_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +22,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initView() {
-        mBtRxjavaRequest = (Button) findViewById(R.id.bt_rxjava_request);
-
-        mBtRxjavaRequest.setOnClickListener(this);
+        bt_rxjava_request = (Button) findViewById(R.id.bt_rxjava_request);
+        bt_rxjava_request.setOnClickListener(this);
+        bt_ret_test = (Button) findViewById(R.id.bt_ret_test);
+        bt_ret_test.setOnClickListener(this);
     }
 
     @Override
@@ -30,6 +33,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.bt_rxjava_request:
                 startActivity(new Intent(this, RxJavaRegisterActivity.class));
+                break;
+            case R.id.bt_ret_test:
+                startActivity(new Intent(this, RetTestActivity.class));
                 break;
             default:
                 break;
---
+++ app/src/main/java/com/qsfan/qtils/utiltest/RetTestActivity.java
@@ -0,0 +1,15 @@
+package com.qsfan.qtils.utiltest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.qsfan.qtils.R;
+
+public class RetTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ret_test);
+    }
+}
